76b90f7384d7d41c26b88099a24dea570d39a3e1
Enrico R. Crema <enrico.crema@gmail.com>
2020-08-03 18:05:26 +0100

    testing environment removed
    This was causing non-reproducible problem on CRAN checks despite rhub based tests did not produce any error.

--- tests/testthat.R
+++
@@ -1,4 +0,0 @@
-library(testthat)
-library(rcarbon)
-
-test_check("rcarbon")
--- tests/testthat/test-calibration.R
+++
@@ -1,5 +0,0 @@
-test_that("multiplication works", {
-  expect_equal_to_reference(rcarbon::calibrate(5000,50), "cal_5000_50.RDS")
-  expect_equal_to_reference(rcarbon::calibrate(5000,50, ncores=2), "cal_5000_50.RDS")
-
-})
